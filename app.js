9a39d2bc84d6d42ed188b80ad634495c1a887189
Miao Yueh Su <bluesmy1020@gmail.com>
2020-03-04 15:47:00 +0800

    Update index page and post shortUrl route, install body-parser and nanoid

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generate = require('nanoid/generate')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -27,7 +31,28 @@ app.get('/', (req, res) => {
 
 // 新增一筆 short url
 app.post('/shortUrl', (req, res) => {
-  res.send('新增short url')
+  const originalUrl = req.body.originalUrl
+  const host = req.headers.host
+
+  Url.findOne({ originalUrl: originalUrl }).then(url => {
+    if (url) {
+      console.log('Url already shortened')
+      const shortUrl = url.shortUrl
+      const completeShortUrl = `https://${host}/${shortUrl}`
+      res.render('index', { shortUrl: completeShortUrl })
+    } else {
+      const shortUrl = generate('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 5)
+      const url = new Url({
+        originalUrl: req.body.originalUrl,
+        shortUrl: host + '/' + shortUrl
+      })
+      const completeShortUrl = `https://${host}/${shortUrl}`
+      url.save(err => {
+        if (err) return console.error(err)
+        return res.render('index', { shortUrl: completeShortUrl })
+      })
+    }
+  })
 })
 
 // 導向 short url網址
